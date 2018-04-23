795d1f14cea2c4867e5a16a475cd8caf2cc27ed3
liyingfe@cse.msu.edu <liyingfe@cse.msu.edu>
2018-04-23 03:25:00 -0400

    decision tree update
    Co-Authored-By: Shirley Li <liyingfe@msu.edu>

--- randomForest_pac.py
+++ randomForest_pac.py
@@ -1,12 +1,4 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Apr 13 23:41:35 2018
-
-@author: shirley
-"""
-
-# Random Forest Classification
+# LDA
 
 # Importing the libraries
 import numpy as np
@@ -14,14 +6,13 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('/Users/shirley/Desktop/voice_Kaggle.csv')
-X= dataset.iloc[:,0:-1]
-y= dataset.iloc[:,-1]
+dataset = pd.read_csv('/Users/Shirley/Desktop/Wine.csv')
+X = dataset.iloc[:, 0:13].values
+y = dataset.iloc[:, 13].values
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
-
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
@@ -29,22 +20,58 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-# After scaling
-from sklearn.ensemble import RandomForestClassifier
-from sklearn import metrics
-classifier = RandomForestClassifier(n_estimators = 30, min_samples_split = 2, min_samples_leaf = 5, criterion = 'gini', random_state =0)
+# Applying LDA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+lda = LDA(n_components = 2)
+X_train = lda.fit_transform(X_train, y_train)
+X_test = lda.transform(X_test)
+plt.plot(np.cumsum(X_train.explained_variance_ratio_))
+
+
+# Fitting Logistic Regression to the Training set
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0)
 classifier.fit(X_train, y_train)
 
-prediction = classifier.predict(X_test)
-accuracy = metrics.accuracy_score(y_test,prediction)
-print(accuracy)
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
 
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
 
-from sklearn.ensemble import RandomForestClassifier
-from sklearn import metrics
-classifier = RandomForestClassifier(n_estimators = 30, min_samples_split = 2, min_samples_leaf = 5, criterion = 'entropy', random_state =0)
-classifier.fit(X_train, y_train)
+# Visualising the Training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green', 'blue')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green', 'blue'))(i), label = j)
+plt.title('Logistic Regression (Training set)')
+plt.xlabel('LD1')
+plt.ylabel('LD2')
+plt.legend()
+plt.show()
 
-prediction = classifier.predict(X_test)
-accuracy = metrics.accuracy_score(y_test,prediction)
-print(accuracy)
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green', 'blue')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green', 'blue'))(i), label = j)
+plt.title('Logistic Regression (Test set)')
+plt.xlabel('LD1')
+plt.ylabel('LD2')
+plt.legend()
+plt.show()
